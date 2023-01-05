c013054ba4e6915febb5702fbf7f1386f29fe4f2
Akku125478 <akashpartake098@gmail.com>
2023-01-05 12:17:18 +0530

    medicine-component-implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,25 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-
+import { MedincineHomeComponent } from './components/medincine-home/medincine-home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { TopdealsComponent } from './components/topdeals/topdeals.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    MedincineHomeComponent,
+    TopdealsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/medincine-home/medincine-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-medincine-home',
+  templateUrl: './medincine-home.component.html',
+  styleUrls: ['./medincine-home.component.scss']
+})
+export class MedincineHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
